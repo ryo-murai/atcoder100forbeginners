21e030e9e5e1eb6cf77066851320d78d84416ccd
ryo-murai <murai.ry@gmail.com>
2022-04-30 17:51:37 +0900

    refine #1

--- src/01_howmany_ways.py
+++ src/01_howmany_ways.py
@@ -1,18 +1,16 @@
+import itertools
+
+
 def find(n, x):
     result = []
-    for n1 in range(1, n - 1):
-        for n2 in range(n1 + 1, n):
-            for n3 in range(n2 + 1, n + 1):
-                # print(f"{n1} {n2} {n3}")
-                if n1 + n2 + n3 == x:
-                    result.append((n1, n2, n3))
-                    # print(f"{n1} {n2} {n3}")
+    for n1, n2, n3 in itertools.combinations(range(1, n + 1), 3):
+        if n1 + n2 + n3 == x:
+            result.append((n1, n2, n3))
     print(f"{len(result)}")
 
 
 while True:
-    (nx, xx) = input().split((" "))
-    (n, x) = int(nx), int(xx)
+    n, x = map(int, input().split(" "))
     if n == 0 and x == 0:
         break
     find(n, x)
